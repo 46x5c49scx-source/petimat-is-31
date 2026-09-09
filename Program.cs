edb692ea2db4b3909e35a54554437df47d62bc75
Тучаева Петимат <your.email@example.com>
2026-09-09 13:43:38 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Шаг 1");
+Console.WriteLine("Шаг 2");
+Console.WriteLine("Шаг 3");
+// Выполняется строго сверху вниз
+{
+    int x = 5;
+    int y = 10;
+    int sum = x + y;
+    Console.WriteLine($"Сумма: {sum}");
+}
+// После выполнения блока переменные x, y, sum недоступны
